7e00fea34359e4f75b1862ba42d109837e06270b
unknown <tson3988@gmail.com>
2025-10-22 22:37:26 +0700

    khoi tao project

---
+++ QLKhachSanApi/DAL/DatabaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace QLKhachSanApi.DAL
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("HotelDb");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+
+}
