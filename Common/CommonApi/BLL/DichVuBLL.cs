a97ed38ef10607c179087d0e4feab4c998f197b4
unknown <tson3988@gmail.com>
2025-12-03 00:03:31 +0700

    BLL conmon

---
+++ Common/CommonApi/BLL/DichVuBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using HotelManagement.Module;
+using DAL;
+
+namespace BLL
+{
+    public class DichVuBLL
+    {
+        private DichVuDAL _dal;
+
+        public DichVuBLL()
+        {
+            _dal = new DichVuDAL();
+        }
+
+        public DataTable GetAll()
+        {
+            return _dal.GetAll();
+        }
+
+        public DataTable GetById(int maDV)
+        {
+            return _dal.GetById(maDV);
+        }
+
+        public string Them(DichVu obj)
+        {
+            if (obj == null)
+                return "Thông tin DichVu không hợp lệ";
+            return _dal.Them(obj);
+        }
+
+        public string Sua(DichVu obj)
+        {
+            if (obj == null)
+                return "Thông tin DichVu không hợp lệ";
+            return _dal.Sua(obj);
+        }
+
+        public string Xoa(int maDV)
+        {
+            return _dal.Xoa(maDV);
+        }
+    }
+}
---
+++ Common/CommonApi/BLL/LoaiPhongBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using HotelManagement.Module;
+using DAL;
+
+namespace BLL
+{
+    public class LoaiPhongBLL
+    {
+        private LoaiPhongDAL _dal;
+
+        public LoaiPhongBLL()
+        {
+            _dal = new LoaiPhongDAL();
+        }
+
+        public DataTable GetAll()
+        {
+            return _dal.GetAll();
+        }
+
+        public DataTable GetById(int maLoaiPhong)
+        {
+            return _dal.GetById(maLoaiPhong);
+        }
+
+        public string Them(LoaiPhong obj)
+        {
+            if (obj == null)
+                return "Thông tin LoaiPhong không hợp lệ";
+            return _dal.Them(obj);
+        }
+
+        public string Sua(LoaiPhong obj)
+        {
+            if (obj == null)
+                return "Thông tin LoaiPhong không hợp lệ";
+            return _dal.Sua(obj);
+        }
+
+        public string Xoa(int maLoaiPhong)
+        {
+            return _dal.Xoa(maLoaiPhong);
+        }
+    }
+}
